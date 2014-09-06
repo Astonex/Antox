e16b86dae86867fecba91ea9ae45de78156e5f8c
olologin <olologin@gmail.com>
2014-09-06 16:40:49 +0000

    Added pending intent to MainActivity

--- app/src/main/java/im/tox/antox/tox/ToxDoService.java
+++ app/src/main/java/im/tox/antox/tox/ToxDoService.java
@@ -1,6 +1,7 @@
 package im.tox.antox.tox;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import im.tox.antox.R;
+import im.tox.antox.activities.MainActivity;
 import im.tox.jtoxcore.ToxException;
 
 public class ToxDoService extends Service {
@@ -17,53 +19,42 @@ public class ToxDoService extends Service {
     private static final String TAG = "im.tox.antox.tox.ToxDoService";
 
     private static ToxSingleton toxSingleton = ToxSingleton.getInstance();
-    private boolean isRunning = false;
-    private Notification mNotification;
+    private PowerManager.WakeLock serviceWL;
+    private Context ctx;
 
     public ToxDoService() {
         super();
     }
 
-
     @Override
     public void onCreate() {
-        Log.d("ToxDoService", "onCreate");
+        this.ctx = getApplicationContext();
+        Log.d(TAG, "onCreate");
         if (!toxSingleton.isInited) {
-            toxSingleton.initTox(getApplicationContext());
-            Log.d("ToxDoService", "Initting ToxSingleton");
+            toxSingleton.initTox(this.ctx);
+            Log.d(TAG, "Initting ToxSingleton");
         }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, final int id) {
-        Log.d("ToxDoService", "onStart");
-        if (this.isRunning) return START_NOT_STICKY;
-
-        this.isRunning = true;
 
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        final PowerManager.WakeLock serviceWL =
+        this.serviceWL =
                 pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Antox Service Wakelock");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(ctx, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_actionbar)
                         .setContentTitle("Antox service is running")
-                        .setDefaults(Notification.DEFAULT_ALL);
-
-        mNotification = mBuilder.build();
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setContentIntent(pendingIntent);
 
         Thread doThread = new Thread() {
             @Override
             public void run() {
                 /* Praise the sun */
-                serviceWL.acquire();
-                while (isRunning) {
+                while (true) {
                     try {
                         Thread.sleep(toxSingleton.jTox.doToxInterval());
                         toxSingleton.jTox.doTox();
@@ -75,18 +66,26 @@ public class ToxDoService extends Service {
                         e.printStackTrace();
                     }
                 }
-                serviceWL.release();
             }
         };
-        startForeground(id, mNotification);
+        serviceWL.acquire();
+        startForeground(1, mBuilder.build());
         doThread.start();
+    }
 
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, final int id) {
         return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        this.isRunning = false;
+        Log.d(TAG, "onDestroy() called");
         stopForeground(true);
         toxSingleton.isInited = false;
         try {
@@ -95,7 +94,7 @@ public class ToxDoService extends Service {
             e.printStackTrace();
         }
         toxSingleton = null;
-        Log.d("ToxDoService", "onDestroy() called");
+        serviceWL.release();
         super.onDestroy();
     }
 }
